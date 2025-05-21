c2ac84a164fbf3e11f62fc20df2757bce627867e
juan jose <juanj.suarezm@uqvirtual.edu.co>
2025-05-21 15:00:36 -0500

    tabla de alojamientos

--- src/main/java/co/edu/uniquindio/bookyourstay/controladores/GestionOfertasControlador.java
+++ src/main/java/co/edu/uniquindio/bookyourstay/controladores/GestionOfertasControlador.java
@@ -1,4 +1,11 @@
 package co.edu.uniquindio.bookyourstay.controladores;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
 public class GestionOfertasControlador {
+
+    @FXML
+    private TableView tablaOfertas;
+
 }
